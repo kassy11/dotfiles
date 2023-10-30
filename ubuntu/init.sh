3c889a5c6fdef6f0ea8d330f6c169617b2b054ae
kassy11 <kotaro11kassy@gmail.com>
2023-10-30 15:38:15 +0900

    add install zplug, rustup and starship

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -3,6 +3,17 @@
 printf '\033[32m%s\033[m\n' 'Remove some files for symlinks'
 rm -rf ~/.zshrc ~/.zprofile ~/.gitconfig ~/.vimrc ~/.commit_template ~/.config/starship.toml
 
+cd
+# rustup
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# zplug
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+# starship
+curl -sS https://starship.rs/install.sh | sh
+
+# back to dotfiles
+cd -
+
 printf '\033[32m%s\033[m\n' 'Make symbolic links...'
 ln -sf $(pwd)/.zshrc ~/.zshrc
 ln -sf $(pwd)/.zprofile ~/.zprofile
@@ -11,7 +22,6 @@ ln -sf $(pwd)/.gitignore_global ~/.gitignore_global
 ln -sf $(pwd)/.vimrc ~/.vimrc
 ln -sf $(pwd)/.commit_template ~/.commit_template
 ln -s  $(pwd)/.config/starship.toml ~/.config/starship.toml
-ln -s $(pwd)/clean.sh ~/clean.sh
 
 printf '\033[32m%s\033[m\n' 'Source .zshrc...'
 source ~/.zshrc
